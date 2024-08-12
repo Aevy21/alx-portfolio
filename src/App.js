6d2bda65677dad0149d5304a72eefb00fafa3610
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-08-12 13:19:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage/Homepage.js';  // Adjusted path
 import './components/Homepage/Homepage.css';  // Import your CSS file
+import SignIn from './components/SignIn/SignInForm.js';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        {/* Add other routes here */}
-      </Routes>
+      <div className="App">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
+
+
 
 export default App;
 

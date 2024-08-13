e253e9e2f898208a653d7e5edd5bbba52add9f9a
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-08-13 16:25:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import Homepage from './components/Homepage/Homepage'; // Adjusted path without '.js'
 import './components/Homepage/Homepage.css'; 
 import SignIn from './components/SignIn/SignInForm'; 

a76e37b8e9153440b56224dbc377d191aac517c8
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-08-13 14:51:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './components/Homepage/Homepage'; // Adjusted path without 
 import './components/Homepage/Homepage.css'; 
 import SignIn from './components/SignIn/SignInForm'; 
 import SignUp from './components/SignUp/SignUpForm'; 
-import AboutUs from './components/Aboutus/AboutUs'; 
+import About from './components/Aboutus/AboutUs'; 
 import Policy from '/components/Policy/Policies';
 import Policy from '/components/Help/Helppage';
 
@@ -16,7 +16,7 @@ const App = () => {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp/>} />
-                <Route path="/aboutus" element={<AboutUs/>} />
+                <Route path="/aboutus" element={<About/>} />
                 <Route path="/policy" element={<Policy/>} />
                 <Route path="/help" element={<Help/>} />
                 {/* Add other routes as necessary */}

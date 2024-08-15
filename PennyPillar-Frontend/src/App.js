d6ccaed72a2cc4aa85394dab12418704c6f884e5
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-08-15 14:45:13 +0200

    Update App.js

--- PennyPillar-Frontend/src/App.js
+++ PennyPillar-Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Homepage from './components/Homepage/Homepage'; 
 import './components/Homepage/Homepage.css'; 
